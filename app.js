6ecd17d0e730cd9854ef0068ce63d4e8af13fd9a
kevimford <kevinmford@yahoo.com>
2017-04-13 18:09:12 -0700

    continued making layout of page several minor changes made

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-use strict
-$( document ).ready(){
-  
-}
+"use strict"
+$(document).ready(function() {
+
+  $('.menu-item').click(function() {
+    $('.menu-item').removeClass('active');
+    $(this).addClass('active');
+    var selected = $(this).text();
+    console.log(selected);
+  });
+
+});
