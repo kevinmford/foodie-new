c98b1f6a5b40badbf6dad38ca3a564f35755bed4
kevimford <kevinmford@yahoo.com>
2017-05-08 17:34:46 -0700

    made social buttons appear on individual card hovers still apearing all at first

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 "use strict"
 $(document).ready(function() {
-
-  $('.menu-item').click(function() {
-    $('.menu-item').removeClass('active');
-    $(this).addClass('active');
-    var selected = $(this).text();
-    console.log(selected);
-  });
-  $( ".whats-cooking-item" )
+  $( "#whats-cooking-1" )
     .on("mouseenter", function() {
-      $(".social-buttons").show();
+      $(".button-set-1").show();
     })
     .on("mouseleave", function() {
-    $(".social-buttons").hide();
+    $(".button-set-1").hide();
+  });
+  $( "#whats-cooking-2" )
+    .on("mouseenter", function() {
+      $(".button-set-2").show();
+    })
+    .on("mouseleave", function() {
+    $(".button-set-2").hide();
+  });
+  $( "#whats-cooking-3" )
+    .on("mouseenter", function() {
+      $(".button-set-3").show();
+    })
+    .on("mouseleave", function() {
+    $(".button-set-3").hide();
+  });
+  $( "#whats-cooking-4" )
+    .on("mouseenter", function() {
+      $(".button-set-4").show();
+    })
+    .on("mouseleave", function() {
+    $(".button-set-4").hide();
   });
 });
